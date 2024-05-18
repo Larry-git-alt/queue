a359e2cf8f5e25000b41f186cae33dcf7ac3196a
fengkaifeng <project@fkf.cn>
2024-05-18 20:35:20 +0800

    online-judge  2024/05/18  @author FrankieFly

---
+++ Queue-online-judge/src/main/java/cn/queue/online_judge/OnlineJudgeApplication.java
@@ -0,0 +1,20 @@
+package cn.queue.online_judge;
+
+import cn.queue.common.annnotation.LarryController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class OnlineJudgeApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(OnlineJudgeApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(OnlineJudgeApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("{} 启动成功!!", env.getProperty("spring.application.name"));
+        LOG.info("{} 地址:\thttp://127.0.0.1:{}", env.getProperty("spring.application.name"), env.getProperty("server.port"));
+    }
+}
