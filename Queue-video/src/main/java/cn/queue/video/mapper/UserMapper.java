f535600462e5e8ddc3d1c353fa2d53871c30f48e
Ccoo <2574789080@qq.com>
2024-05-22 20:04:18 +0800

    数据同步Canal+整合Easy-Es

---
+++ Queue-video/src/main/java/cn/queue/video/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+//package cn.queue.video.mapper;
+//
+//import cn.queue.video.domain.entity.User;
+//import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+//
+///**
+// * @author Ccoo
+// * 2024/5/22
+// */
+//public class UserMapper extends BaseMapper<User> {
+//}
