195441dd615ca0bafaa0c6bdfd1d53091f407b8d
Larry-git-alt <139796123+Larry-git-alt@users.noreply.github.com>
2024-06-03 21:17:36 +0800

    修改部分配置

--- Queue-Im-core/src/main/java/cn/queue/imcore/config/RedissonManager.java
+++ Queue-Im-core/src/main/java/cn/queue/imcore/config/RedissonManager.java
@@ -1,24 +1,24 @@
-package cn.queue.imcore.config;
-
-
-import org.redisson.api.RedissonClient;
-
-/**
- * Redisson 客户端管理类
- * @author BanTanger 半糖
- * @Date 2023/3/24 22:07
- */
-public class RedissonManager {
-
-    private static RedissonClient redissonClient;
-
-    public static void init(RedisConfig redisConfig) {
-        SingleClientStrategy singleClientStrategy = new SingleClientStrategy();
-        redissonClient = singleClientStrategy.getRedissonClient(redisConfig);
-    }
-
-    public static RedissonClient getRedissonClient() {
-        return redissonClient;
-    }
-
-}
+//package cn.queue.imcore.config;
+//
+//
+//import org.redisson.api.RedissonClient;
+//
+///**
+// * Redisson 客户端管理类
+// * @author BanTanger 半糖
+// * @Date 2023/3/24 22:07
+// */
+//public class RedissonManager {
+//
+//    private static RedissonClient redissonClient;
+//
+//    public static void init(RedisConfig redisConfig) {
+//        SingleClientStrategy singleClientStrategy = new SingleClientStrategy();
+//        redissonClient = singleClientStrategy.getRedissonClient(redisConfig);
+//    }
+//
+//    public static RedissonClient getRedissonClient() {
+//        return redissonClient;
+//    }
+//
+//}
